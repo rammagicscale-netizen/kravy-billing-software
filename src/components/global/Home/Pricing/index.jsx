60aeb12c7089a166628c52a88f582e2e502084a8
vikas <vikasvikas9095@gmail.com>
2026-02-28 15:03:48 +0530

    Your message

--- src/components/global/Home/Pricing/index.jsx
+++ src/components/global/Home/Pricing/index.jsx
@@ -7,7 +7,7 @@ export const plans = [
   {
     key: "starter",
     name: "Starter Plan",
-    price: "₹1249",
+    price: "₹1",
     description: "Perfect to start billing digitally for small businesses.",
     features: [
       "Up to 200 invoices / month",
